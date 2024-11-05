f4cc4d21b8f19ba80e0f9dd0a3a6952408251f3c
guilherme.monteiro.espim@gmail.com <guilherme.monteiro.espim@gmail.com>
2024-11-05 08:10:40 -0300

    students

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import TitleHeader from "./utils/components/Headers/TitleHeader/TitleHeader";
 import SideMenu from "./utils/components/Menus/SideMenu/SideMenu";
 import "./App.scss";
-import Dashboard from "./pages/Dashoard/Dashboard";
+import { Outlet } from "react-router-dom";
 
 function App() {
   return (
@@ -9,9 +9,7 @@ function App() {
         <SideMenu />
         <section className="page-content">
           <TitleHeader title="Dashboard" />
-          <Dashboard /> 
-          {/* <LineChart /> */}
-          {/* <DoughnutChart percentage={80} /> */}
+          <Outlet />
         </section>
     </section>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,29 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./assets/css/styles.scss";
+import { Navigate, RouterProvider, createBrowserRouter, redirect } from "react-router-dom";
+import Dashboard from "./pages/Dashoard/Dashboard.jsx";
+
+const routes = createBrowserRouter([
+  {
+    path: "/",
+    errorElement: <Error />,
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Navigate to="/dashboard" replace />
+      },
+      {
+        path: "/dashboard",
+        element: <Dashboard />
+      }
+    ]
+  }
+]);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={routes} />
   </StrictMode>,
 );
